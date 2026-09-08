9981c6cf9b9c9bd8041013ba5f2638fc463df394
Meykandan <meykandan123@gmail.com>
2026-09-08 23:12:51 +0530

    Clear COOP error and resolve accessibility label association issues

--- server.js
+++ server.js
@@ -27,10 +27,6 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 // Middleware
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
-  next();
-});
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));

decd202e76762d37cfdc8f06ef0cff6be2e0210b
Meykandan <meykandan123@gmail.com>
2026-09-08 23:03:35 +0530

    Fix Cross-Origin-Opener-Policy header to same-origin-allow-popups for Firebase Auth popups

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (!fs.existsSync(uploadsDir)) {
 
 // Middleware
 app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
   next();
 });
 app.use(cors());
